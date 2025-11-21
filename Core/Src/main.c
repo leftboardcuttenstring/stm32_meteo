38e86019583e554664aa40b00b18f3cab6d7df1f
veldigfastpack <michael.a.strangewood@gmail.com>
2025-11-21 09:00:59 +0300

    new branch: stm32_meteo_modbus

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,10 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "date/date.h"
-#include "transmit_and_recieve_control/transmit_and_recieve_control.h"
-#include "init_peripherials/init_peripherials.h"
-#include "date/date.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,54 +40,19 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-
 RTC_HandleTypeDef hrtc;
-
-TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
-uint16_t lcd1604_addr = 0x27 << (uint16_t)1;
-uint16_t bmp180_addr = 0x77 << (uint16_t)1;
-uint16_t aht10_addr = 0x38 << (uint16_t)1;
-
-uint8_t calib_data[22] = {0};
-uint8_t aht10_init_command[1] = {0xe1};
-
-extern uint8_t cmd;
-
-uint8_t AHT10_RX_Data[6] = {0};
-float AHT10_Temperature; 
-float AHT10_Humidity = 0.0;
-
-uint8_t AHT10_TmpHum_Cmd[3] = {0xAC, 0x33, 0x00};
-
-uint8_t temperature_buf[2];
-uint8_t pressure_buf[3];
-char GLOBAL_MESSAGE_BUFFER[80] = {0};
-int16_t AC1, AC2, AC3, B1, B2, MB, MC, MD;
-uint16_t AC4, AC5, AC6;
-int32_t X1 = 0;
-int32_t X2 = 0;
-int32_t B5 = 0; //global stuff
-
-int32_t temperature = 0;
-int32_t pressure = 0;
-
-RTC_TimeTypeDef time;
-RTC_DateTypeDef date;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
 static void MX_RTC_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -129,17 +91,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
   MX_RTC_Init();
   MX_USART2_UART_Init();
-  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-
-  lcd1602_init();
-  bmp180_init();
-  aht10_init(); //strange stuff, but it works
-  init_rtc_once();
-  bmp180_get_global_coefficients();
   
   /* USER CODE END 2 */
 
@@ -175,59 +129,30 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV6;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
-    Error_Handler("Error: an error occurred while executing the 'SystemClock_Config' function (something other)\n");
+    Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
-    Error_Handler("Error: an error occurred while executing the 'SystemClock_Config' function (HSI init)\n");
+    Error_Handler();
   }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler("Error: an error occurred while executing the 'MX_I2C1_Init' function\n");
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
@@ -257,58 +182,11 @@ static void MX_RTC_Init(void)
   hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
   if (HAL_RTC_Init(&hrtc) != HAL_OK)
   {
-    Error_Handler("Error: an error occurred while executing the 'MX_RTC_Init' function\n");
+    Error_Handler();
   }
   /* USER CODE BEGIN RTC_Init 2 */
 
   /* USER CODE END RTC_Init 2 */
-
-}
-
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 720;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 30000;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler("Error: an error occurred while executing the 'MX_TIM3_Init' function (tim3 init)\n");
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler("Error: an error occurred while executing the 'MX_TIM3_Init' function (tim3 init clocksource)\n");
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler("Error: an error occurred while executing the 'MX_TIM3_Init' function (tim3 init master)\n");
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  __HAL_TIM_CLEAR_FLAG(&htim3, TIM_FLAG_UPDATE); 
-
-  /* USER CODE END TIM3_Init 2 */
 
 }
 
@@ -337,7 +215,7 @@ static void MX_USART2_UART_Init(void)
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
   if (HAL_UART_Init(&huart2) != HAL_OK)
   {
-    Error_Handler("Error: an error occurred while executing the 'MX_USART2_UART_Init' function\n");
+    Error_Handler();
   }
   /* USER CODE BEGIN USART2_Init 2 */
 
@@ -352,41 +230,13 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PC7 PC8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 8, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 10, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
   
@@ -401,10 +251,10 @@ static void MX_GPIO_Init(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(char* msg)
+void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  HAL_UART_Transmit(&huart2, (const uint8_t*)msg, strlen((char *)msg), HAL_MAX_DELAY);
+  
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -22,10 +22,7 @@
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdbool.h>
-#include <stdio.h>
-#include "transmit_and_recieve_control/transmit_and_recieve_control.h"
-#include <string.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -35,16 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define HUMIDITY_PUT 19849
-#define HUMIDITY_GET 19949
 
-#define TEMPERATURE_PUT 19950
-#define TEMPERATURE_GET 19969
-
-#define PRESSURE_PUT 19970
-#define PRESSURE_GET 20000
-
-#define OSS 0
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -55,57 +43,10 @@
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
 
-extern uint16_t lcd1604_addr;
-extern uint16_t bmp180_addr;
-extern uint16_t aht10_addr;
-
-int32_t bmp180_temperature_result = 0;
-int32_t bmp180_pressure_result = 0;
-uint32_t aht10_raw_data;
-
-int32_t critical_values_log[5] = {0};
-int critical_values_log_count = 0;
-
-static unsigned int SysTick_20Sec_Counter = 0;
-static unsigned int SysTick_1Sec_Counter_mode = 0;
-static unsigned int SysTick_1Sec_Counter_mode1 = 0;
-static unsigned int SysTick_1Sec_Counter_mode2 = 0;
-
-char hour_modifying_string[10] = {0};
-char minutes_modifying_string[10] = {0};
-
-extern RTC_TimeTypeDef time;
-extern RTC_DateTypeDef date;
-
-RTC_TimeTypeDef local_modifying_time;
-RTC_DateTypeDef local_modifying_date;
-
-extern RTC_HandleTypeDef hrtc;
-extern UART_HandleTypeDef huart2;
-extern int count;
-extern I2C_HandleTypeDef hi2c1;
-extern uint8_t cmd;
-
-extern char GLOBAL_MESSAGE_BUFFER[80];
-
-
-extern uint8_t AHT10_RX_Data[6];
-uint8_t aht10_measurement_command[3] = {0xac, 0x33, 0x00};
-//unsigned int counter_1s = 0;
-
-static unsigned int current_mode = 0;
-
-int data_array_counter = 0;
-
-Data data_array[2];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
-void process_temperature(void);
-void process_pressure(void);
-void process_humidity(void);
-void display_data(void);
 
 /* USER CODE END PFP */
 
@@ -115,7 +56,7 @@ void display_data(void);
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim3;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -243,78 +184,10 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
-  if (critical_values_log_count > 4) {
-    critical_values_log_count = 0;
-  }
+  
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
-
-  if (current_mode == 0) {
-    SysTick_1Sec_Counter_mode1 = 0;
-    SysTick_1Sec_Counter_mode2 = 0;
-    if (SysTick_20Sec_Counter == HUMIDITY_PUT) {
-      HAL_I2C_Master_Transmit(&hi2c1, aht10_addr, (uint8_t*)aht10_measurement_command, 3, HAL_MAX_DELAY);
-    }
-    if (SysTick_20Sec_Counter == HUMIDITY_GET) {
-      process_humidity();
-    }
-    if (SysTick_20Sec_Counter == TEMPERATURE_PUT) {
-      HAL_I2C_Mem_Write(&hi2c1, bmp180_addr, 0xF4, 1, &cmd, 1, HAL_MAX_DELAY);
-    }
-    if (SysTick_20Sec_Counter == TEMPERATURE_GET) {
-      process_temperature();
-    }
-    if (SysTick_20Sec_Counter == PRESSURE_PUT) {
-      uint8_t pressure_cmd = 0x34 + (OSS << 6);
-      HAL_I2C_Mem_Write(&hi2c1, bmp180_addr, 0xF4, 1, &pressure_cmd, sizeof(pressure_cmd), HAL_MAX_DELAY);
-    }
-    if (SysTick_20Sec_Counter == PRESSURE_GET) {
-      SysTick_20Sec_Counter = 0;
-      process_pressure();
-    }
-    SysTick_20Sec_Counter++;
-    if (SysTick_1Sec_Counter_mode == 1000) {
-      SysTick_1Sec_Counter_mode = 0;
-      
-      data_array_counter = (data_array_counter + 1) % 2;
-      data_array[data_array_counter].current_temperature = bmp180_temperature_result;
-      data_array[data_array_counter].current_pressure = bmp180_pressure_result;
-      data_array[data_array_counter].current_humidity = (int32_t)aht10_raw_data;
-
-      HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
-      HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
-      lcd1602_transmit_command(0b10000000);
-      snprintf(GLOBAL_MESSAGE_BUFFER, sizeof(GLOBAL_MESSAGE_BUFFER), "%02d:%02d:%02d", \
-        time.Hours, time.Minutes, time.Seconds);
-      lcd1602_send_string(GLOBAL_MESSAGE_BUFFER);
-      lcd1602_transmit_command(0b11000000);
-
-      snprintf(GLOBAL_MESSAGE_BUFFER, sizeof(GLOBAL_MESSAGE_BUFFER), "%.2ld, %.2ld, %.2f", \
-        (data_array[0].current_pressure + data_array[1].current_pressure) / 2, 
-        (data_array[0].current_temperature + data_array[1].current_temperature) / 2, 
-        ((((float)data_array[0].current_humidity / 1048576.0) * 100.0) + (((float)data_array[1].current_humidity / 1048576.0) * 100.0)) / 2.0);
-
-      lcd1602_send_string(GLOBAL_MESSAGE_BUFFER);
-    }
-    SysTick_1Sec_Counter_mode++;
-  } 
-  if (current_mode == 1) {
-    SysTick_1Sec_Counter_mode = 0;
-    if (SysTick_1Sec_Counter_mode1 == 100) {
-      
-    }
-    SysTick_1Sec_Counter_mode1++;
-  }
-  if (current_mode == 2) {
-    SysTick_1Sec_Counter_mode = 0;
-    SysTick_1Sec_Counter_mode1 = 0;
-    if (SysTick_1Sec_Counter_mode2 == 100) {
-      SysTick_1Sec_Counter_mode2 = 0;
-      
-    }
-    SysTick_1Sec_Counter_mode2++;
-  }
 
   /* USER CODE END SysTick_IRQn 1 */
 }
@@ -326,167 +199,7 @@ void SysTick_Handler(void)
 /* please refer to the startup file (startup_stm32f4xx.s).                    */
 /******************************************************************************/
 
-/**
-  * @brief This function handles EXTI line[9:5] interrupts.
-  */
-void EXTI9_5_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
-
-  /* USER CODE END EXTI9_5_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
-  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
-
-  /* USER CODE END EXTI9_5_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
-  
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
-
-  /* USER CODE END TIM3_IRQn 1 */
-}
-
-/**
-  * @brief This function handles EXTI line[15:10] interrupts.
-  */
-void EXTI15_10_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
-  
-  /* USER CODE END EXTI15_10_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
-  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
-
-  /* USER CODE END EXTI15_10_IRQn 1 */
-}
-
 /* USER CODE BEGIN 1 */
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-  if(htim->Instance == TIM3) {
-    HAL_TIM_Base_Stop_IT(&htim3); 
-    current_mode++;
-    if (current_mode > 2) {
-      current_mode = 0;
-    }
-  }
-}
-
-int modifying_hours = 0;
-int modifying_minutes = 0;
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-  if(GPIO_Pin == GPIO_PIN_8) {
-    current_mode++;
-    if (current_mode > 2) {
-      current_mode = 0;
-    }
-  } else if (GPIO_Pin == GPIO_PIN_7) {
-    if (current_mode == 1) {
-      modifying_hours++;
-      if (modifying_hours >= 23) {
-        modifying_hours = 0;
-      }
-      snprintf(hour_modifying_string, sizeof(hour_modifying_string), "  %d", modifying_hours);
-      lcd1602_transmit_command(0b00000001);
-      lcd1602_transmit_command(0b10000000);
-      lcd1602_send_string(hour_modifying_string);
-    } else if (current_mode == 2) {
-      modifying_minutes++;
-      if (modifying_minutes >= 59) {
-        modifying_minutes = 0;
-      }
-      snprintf(minutes_modifying_string, sizeof(minutes_modifying_string), "  %d", modifying_minutes);
-      lcd1602_transmit_command(0b00000001);
-      lcd1602_transmit_command(0b10000000);
-      lcd1602_send_string(minutes_modifying_string);
-    } else if (current_mode == 0) {
-      RTC_TimeTypeDef sTime = { .Hours = modifying_hours, .Minutes = modifying_minutes, .Seconds = 0 };
-      RTC_DateTypeDef sDate = { .WeekDay = RTC_WEEKDAY_SUNDAY, .Month = RTC_MONTH_OCTOBER,
-                                .Date = 19, .Year = 25 };
-
-      HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
-      HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
-      time.Hours = modifying_hours;
-      time.Minutes = modifying_minutes;
-      time.Seconds = 0;
-    } else {
-      current_mode = 0;
-    }
-  } else {
-    __NOP();
-  }
-}
-
-void process_temperature(void) {
-  bmp180_temperature_result = bmp180_get_temperature();
-
-  if (bmp180_temperature_result / 10.0 > MAX_TEMPERATURE) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The temperature is above the maximum! (38 celsius)\r\n", \
-      sizeof("The temperature is above the maximum! (38 celsius)\r\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = bmp180_temperature_result;
-    critical_values_log_count++;
-  } else if (bmp180_temperature_result / 10.0 < MIN_TEMPERATURE) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The temperature is below the minimum! (-40 celsius)\r\n", \
-      sizeof("The temperature is below the minimum! (-40 celsius)\r\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = bmp180_temperature_result;
-    critical_values_log_count++;
-  }
-}
-
-void process_pressure(void) {
-  bmp180_pressure_result = bmp180_get_pressure() / 133.322f;
-
-  if (bmp180_pressure_result / 133.322f > MAX_PRESSURE) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The pressure is above the maximum! (770 millimeters of mercury)\r\n", \
-      sizeof("The pressure is above the maximum! (770 millimeters of mercury)\r\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = bmp180_pressure_result;
-    critical_values_log_count++;
-  } else if (bmp180_pressure_result / 133.322f < MIN_PRESSURE) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The pressure is below the minimum! (730 millimeters of mercury)\r\n", \
-      sizeof("The pressure is below the minimum! (730 millimeters of mercury)\r\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = bmp180_pressure_result;
-    critical_values_log_count++;
-  }
-}
-
-void process_humidity(void) {
-  HAL_I2C_Master_Receive(&hi2c1, aht10_addr, AHT10_RX_Data, 6, HAL_MAX_DELAY);
-  aht10_raw_data = ((uint32_t)AHT10_RX_Data[1] << 12) | ((uint32_t)AHT10_RX_Data[2] << 4) | (AHT10_RX_Data[3] >> 4);
-
-  if ((float)aht10_raw_data >= 100) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The humidity is above the maximum!\n", \
-      sizeof("The humidity is above the maximum!\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = (uint32_t)aht10_raw_data;
-    critical_values_log_count++;
-  } else if ((float)aht10_raw_data <= 20) {
-    HAL_UART_Transmit(&huart2, (const uint8_t*)"The humidity is below the minimum!\n", \
-      sizeof("The humidity is below the minimum!\n")-1, HAL_MAX_DELAY);
-    critical_values_log[critical_values_log_count] = (uint32_t)aht10_raw_data;
-    critical_values_log_count++;
-  }
-}
-
-void display_data(void) {
-  HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
-  HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
-  lcd1602_transmit_command(0b10000000);
-  snprintf(GLOBAL_MESSAGE_BUFFER, sizeof(GLOBAL_MESSAGE_BUFFER), "%02d:%02d:%02d", \
-    time.Hours, time.Minutes, time.Seconds);
-  lcd1602_send_string(GLOBAL_MESSAGE_BUFFER);
-  lcd1602_transmit_command(0b11000000);
-  snprintf(GLOBAL_MESSAGE_BUFFER, sizeof(GLOBAL_MESSAGE_BUFFER), "%.2ld, %.2ld, %.2f", \
-    bmp180_pressure_result, bmp180_temperature_result, ((float)aht10_raw_data / 1048576.0) * 100.0);
-  lcd1602_send_string(GLOBAL_MESSAGE_BUFFER);
-}
 
 /* USER CODE END 1 */
